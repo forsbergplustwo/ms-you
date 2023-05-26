d295f5ebc4ea80bc15089a6a84b30aa839b6dbec
Bjorn Forsberg <bjorn@forsbergplustwo.com>
2023-05-26 16:57:20 +0200

    Graph improvements + favicon, page title & footer

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,7 @@
 class Measurement < ApplicationRecord
   belongs_to :symptom
+
+  def self.all_by_day_and_severity
+    all.group_by_day(:created_at).sum(:severity).reject { |k, v| v == 0 }
+  end
 end
--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -2,9 +2,19 @@ class Symptom < ApplicationRecord
   enum severity: { gone: 0, mild: 1, moderate: 2, severe: 3 }
   has_many :measurements, dependent: :destroy
 
-  after_save :create_measurement
+  after_create :create_starting_measurement
+  after_update :create_update_measurement
 
-  def create_measurement
-    Measurement.create!(symptom: self, severity: self.severity_before_type_cast)
+  def create_starting_measurement
+    create_measurement(start_date)
   end
+
+  def create_update_measurement
+    create_measurement(Time.now)
+  end
+
+  def create_measurement(date)
+    Measurement.create!(symptom: self, severity: self.severity_before_type_cast, created_at: date, updated_at: date)
+  end
+
 end
